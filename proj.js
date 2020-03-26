78f5f7e3915a5db364bef4e72eb42bd436d280a7
oma16 <48384861+oma16@users.noreply.github.com>
2020-03-26 13:05:17 +0100

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,17 @@
+var name = "Alli Mariam Titilope";
+document.write(name); 
+document.write("<br>");
+
+var courses = ["html", "css", "javascript", "php", "design"];
+document.write(courses);
+document.write("<br>");
+
+var i;
+
+for (i = 1; i<=200; i++){
+    if(i % 2 !== 0){
+        document.write(i + '<br>');
+    }
+    
+}
+
